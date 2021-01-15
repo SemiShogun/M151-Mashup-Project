5abff9c054c913eb7797de0ef880c23349a679cf
SemiShogun <nectoj@gmail.com>
2021-01-15 09:14:50 +0100

    Added Cors for backend

--- backend/src/main/java/ch/bbw/jl/idunno/controller/DadJokeController.java
+++ backend/src/main/java/ch/bbw/jl/idunno/controller/DadJokeController.java
@@ -3,12 +3,10 @@ package ch.bbw.jl.idunno.controller;
 import ch.bbw.jl.idunno.dao.DadJokeDao;
 import ch.bbw.jl.idunno.entity.DadJoke;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8081")
 @RequestMapping("/dadjoke")
 public class DadJokeController {
 
